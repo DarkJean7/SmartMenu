66209b96f3098c2ba6c204303680f4a1dd32d15f
DarkJean7 <92082817+DarkJean7@users.noreply.github.com>
2021-11-29 01:45:01 -0400

    Add files via upload

---
+++ SmartMenu.py
@@ -0,0 +1,23 @@
+
+def Menu(file):
+    file = open(file, 'r')
+    food = []
+    ingredients = []
+    for line in file:
+        line = line.rstrip()
+        if line != "" and line[0].isupper() and not line.endswith(':'): #food
+            food.append(line)
+        elif line != "" and line[0].islower() and line not in ingredients: #ingredients
+            ingredients.append(line)
+            ingredients = sorted(ingredients)
+    print(food, ingredients)
+
+
+
+
+
+
+
+
+
+print(Menu("spam.txt"))
