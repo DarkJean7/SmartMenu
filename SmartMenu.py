c6647f444c25ac48c30a86ed88684943c5c2a825
Jeank Dark <jeankgonzalez47@gmail.com>
2021-11-30 01:47:08 -0400

    Items Can Change Availability

--- SmartMenu.py
+++ SmartMenu.py
@@ -30,9 +30,25 @@ def Available():
             notAvailable.append(fL[k])
     return available, notAvailable
 
+def ChangeAvailability():
+    print("The Available items are: ")
+    for i in range(len(Available()[0])):
+        print(Available()[0][i])
+    print("The out of stock items are: ")
+    for k in range(len(Available()[1])):
+        print(Available()[1][k])
+    item = input("Enter the name of the item to change: ")
+    status = input("If the item is out of stock enter F, in the contrary enter T: ")
+    if item in fL:
+        if status.upper() == "F":
+            restaurant[item].insert(0, False)
+        elif status.upper() == "T":
+            restaurant[item].insert(0, True)
+
 restaurant = Menu("spam.txt")
 fL = FoodList("spam.txt")
 
 print(restaurant)
 print(len(fL))
 print(Available())
+print(ChangeAvailability())
