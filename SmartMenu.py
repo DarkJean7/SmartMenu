f87b4d900c9bf3bb63c9e8b55cf0a11f19d6fabe
Jeank Dark <jeankgonzalez47@gmail.com>
2021-11-30 01:14:10 -0400

    Done

--- SmartMenu.py
+++ SmartMenu.py
@@ -6,9 +6,33 @@ def Menu(file):
         line = line.rstrip()
         if line != "" and line[0].isupper() and not line.endswith(':'): #food
             f = line
-            food.update({line : []})
+            food.update({line : [True]})
         elif line != "" and line[0].islower(): #ingredients
             food[f].append(line)
     return food
 
-print(Menu("spam.txt"))
+def FoodList(file):
+    file = open(file, 'r')
+    foodList = []
+    for line in file:
+        line = line.rstrip()
+        if line != "" and line[0].isupper() and not line.endswith(':'):
+            foodList.append(line)
+    return foodList
+
+def Available():
+    available = []
+    notAvailable = []
+    for k in range(len(fL)):
+        if restaurant.get(fL[k])[0]:
+            available.append(fL[k])
+        else:
+            notAvailable.append(fL[k])
+    return available, notAvailable
+
+restaurant = Menu("spam.txt")
+fL = FoodList("spam.txt")
+
+print(restaurant)
+print(len(fL))
+print(Available())
